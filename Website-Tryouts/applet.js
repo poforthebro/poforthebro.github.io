538b1e6cbe3a5f106ed7ba9e4f1590851c611918
poforthebro <likutis2009@gmail.com>
2022-01-12 11:46:09 +0100

    Added Website Nr.1
    Made by me, Website is made with HTML CSS and a bit of Javascript.

---
+++ Website-Tryouts/applet.js
@@ -0,0 +1,54 @@
+const menu = document.querySelector("#mobile_menu");
+const menuLinks = document.querySelector(".navbar_menu");
+const navLogo = document.querySelector("#navbar_logo");
+
+//MENU FOR MOBILE
+const mobileMenu = () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+};
+
+menu.addEventListener("click", mobileMenu);
+
+// Show the active menu when scrolling
+const highlightMenu = () => {
+  const elements = document.querySelector(".highlight");
+  const homeMenu = document.querySelector("#home-page");
+  const aboutMenu = document.querySelector("#about-page");
+  const servicesMenu = document.querySelector("#services-page");
+  let scrollPos = window.scrollY;
+
+  //adds highlight class to menu items
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add("highlight");
+    homeMenu.classList.remove("highlight");
+    servicesMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    aboutMenu.classList.remove("highlight");
+    servicesMenu.classList.add("highlight");
+    return;
+  }
+
+  if ((elements && window.innerWidth < 960 && scrollPos < 600) || elements) {
+    elements.classList.remove("highlight");
+  }
+};
+
+window.addEventListener("scroll", highlightMenu);
+window.addEventListener("click", highlightMenu);
+
+//hides the mobile menu when you click on an item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector('.is-active')
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle('is-active')
+    menuLinks.classList.remove('active')
+  }
+}
+menuLinks.addEventListener('click', hideMobileMenu)
+navLogo.addEventListener('click', hideMobileMenu)
