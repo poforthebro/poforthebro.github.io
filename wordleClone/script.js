6e5d63df80cebf040074749f08b7b0700f176ff2
poforthebro <likutis2009@gmail.com>
2022-02-07 22:41:03 +0100

    fixed this not working on stationary webplace

--- wordleClone/script.js
+++ wordleClone/script.js
@@ -2314,7 +2314,7 @@ const targetWords = [
   "artsy",
   "rural",
   "shave",
-]
+];
 
 const dictionary = [
   "aahed",
@@ -15289,91 +15289,94 @@ const dictionary = [
   "artsy",
   "rural",
   "shave",
-]
-const WORD_LENGTH = 5
+];
+const WORD_LENGTH = 5;
 const alertContainer = document.querySelector("[data-alert-container]");
-const guessGrid = document.querySelector("[data-guess-grid]")
-const offsetFromDate = new Date(2022, 0, 1)
-const msOffset = Date.now() - offsetFromDate
-const dayOffset = msOffset / 1000 / 60 / 60 / 24
-console.log(dayOffset)
-const targetWord = targetWords[Math.floor(dayOffset)]
-startInteraction()
+const guessGrid = document.querySelector("[data-guess-grid]");
+const offsetFromDate = new Date(2022, 0, 1);
+const msOffset = Date.now() - offsetFromDate;
+const dayOffset = msOffset / 1000 / 60 / 60 / 24;
+console.log(dayOffset);
+const targetWord = targetWords[Math.floor(dayOffset)];
+startInteraction();
 
 function startInteraction() {
-    document.addEventListener("click", handleMouseClick)
-    document.addEventListener("keydown", handleKeyPress)
+  document.addEventListener("click", handleMouseClick);
+  document.addEventListener("keydown", handleKeyPress);
 }
 function stopInteraction() {
-    document.removeEventListener("click", handleMouseClick);
-    document.removeEventListener("keydown", handleKeyPress); 
+  document.removeEventListener("click", handleMouseClick);
+  document.removeEventListener("keydown", handleKeyPress);
 }
 function handleMouseClick(e) {
-    if (e.target.matches("[data-key]")) {
-        pressKey(e.target.dataset.key)
-        return
-    }
+  if (e.target.matches("[data-key]")) {
+    pressKey(e.target.dataset.key);
+    return;
+  }
 
-    if (e.target.matches("[data-enter]")) {
-    submitGuess() //!FUNCTION ISNT MADE DUMBASS
-        return
-    }
+  if (e.target.matches("[data-enter]")) {
+    submitGuess(); //!FUNCTION ISNT MADE DUMBASS
+    return;
+  }
 
-    if (e.target.matches("[data-delete]")) {
-        deleteKey()
-        return
-    }
+  if (e.target.matches("[data-delete]")) {
+    deleteKey();
+    return;
+  }
 }
 function handleKeyPress(e) {
-    if (e.key === "Enter") {
-        submitGuess()
-        return
-    }
-    if (e.key === "Backspace" || e.key === "Delete") {
-        deleteKey()
-        return
-    }
-    if (e.key.match(/^[a-z]$/)) {
-        pressKey(e.key)
-        return
-    }
+  if (e.key === "Enter") {
+    submitGuess();
+    return;
+  }
+  if (e.key === "Backspace" || e.key === "Delete") {
+    deleteKey();
+    return;
+  }
+  if (e.key.match(/^[a-z]$/)) {
+    pressKey(e.key);
+    return;
+  }
 }
-function pressKey(key) { //*this shit do the press changes to board an
-const activeTiles = getActiveTiles()
-if (activeTiles.length >= WORD_LENGTH) return
-const nextTile = guessGrid.querySelector(":not([data-letter])")
-nextTile.dataset.letter = key.toLowerCase()
-nextTile.textContent = key
-nextTile.dataset.state = "active"
+function pressKey(key) {
+  //*this shit do the press changes to board an
+  const activeTiles = getActiveTiles();
+  if (activeTiles.length >= WORD_LENGTH) return;
+  const nextTile = guessGrid.querySelector(":not([data-letter])");
+  nextTile.dataset.letter = key.toLowerCase();
+  nextTile.textContent = key;
+  nextTile.dataset.state = "active";
 }
 function deleteKey() {
-    const activeTiles = getActiveTiles()
-    const lastTile = activeTiles[activeTiles.length - 1]
-    if (lastTile == null) return
-    lastTile.textContent = ""
-    delete lastTile.dataset.state
-    delete lastTile.dataset.letter
+  const activeTiles = getActiveTiles();
+  const lastTile = activeTiles[activeTiles.length - 1];
+  if (lastTile == null) return;
+  lastTile.textContent = "";
+  delete lastTile.dataset.state;
+  delete lastTile.dataset.letter;
 }
 function submitGuess() {
-  const activeTiles = [...getActiveTiles()]
+  const activeTiles = [...getActiveTiles()];
   if (activeTiles.length !== WORD_LENGTH) {
-    showAlert("Not Enough Letters Dumbo!")
-    shakeTiles(activeTiles)
-    return
+    showAlert("Not Enough Letters Dumbo!");
+    shakeTiles(activeTiles);
+    return;
   }
 }
 function getActiveTiles() {
-    return guessGrid.querySelectorAll('[data-state="active"]')
+  return guessGrid.querySelectorAll('[data-state="active"]');
 }
 function showAlert(message, duration = 1000) {
-  const alert = document.createElement("div")
-  alert.textContent = message
-  alert.classList.add ("alert")
-  alertContainer.prepend(alert)
-  if (duration == null) return
+  const alert = document.createElement("div");
+  alert.textContent = message;
+  alert.classList.add("alert");
+  alertContainer.prepend(alert);
+  if (duration == null) return;
 
   setTimeout(() => {
-    alert.classList.add("hide")
-    alert.addEventListener("transitionend", () => {alert.remove()})
-  }, duration)
+    alert.classList.add("hide");
+    alert.addEventListener("transitionend", () => {
+      alert.remove();
+    });
+  }, duration);
 }
