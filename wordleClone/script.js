e8639649eb7cc76eee611a340ec0cb72cfa4958c
poforthebro <likutis2009@gmail.com>
2022-02-07 22:31:12 +0100

    Made Alerts Work

--- wordleClone/script.js
+++ wordleClone/script.js
@@ -15291,7 +15291,13 @@ const dictionary = [
   "shave",
 ]
 const WORD_LENGTH = 5
+const alertContainer = document.querySelector("[data-alert-container]");
 const guessGrid = document.querySelector("[data-guess-grid]")
+const offsetFromDate = new Date(2022, 0, 1)
+const msOffset = Date.now() - offsetFromDate
+const dayOffset = msOffset / 1000 / 60 / 60 / 24
+console.log(dayOffset)
+const targetWord = targetWords[Math.floor(dayOffset)]
 startInteraction()
 
 function startInteraction() {
@@ -15348,6 +15354,26 @@ function deleteKey() {
     delete lastTile.dataset.state
     delete lastTile.dataset.letter
 }
+function submitGuess() {
+  const activeTiles = [...getActiveTiles()]
+  if (activeTiles.length !== WORD_LENGTH) {
+    showAlert("Not Enough Letters Dumbo!")
+    shakeTiles(activeTiles)
+    return
+  }
+}
 function getActiveTiles() {
     return guessGrid.querySelectorAll('[data-state="active"]')
+}
+function showAlert(message, duration = 1000) {
+  const alert = document.createElement("div")
+  alert.textContent = message
+  alert.classList.add ("alert")
+  alertContainer.prepend(alert)
+  if (duration == null) return
+
+  setTimeout(() => {
+    alert.classList.add("hide")
+    alert.addEventListener("transitionend", () => {alert.remove()})
+  }, duration)
 }
